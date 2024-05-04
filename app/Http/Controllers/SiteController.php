b6d9bb7e2b547b5d82744472f99d522bc05bcd89
Enrique Oliveira <enrique.oliveira147@gmail.com>
2024-05-04 18:13:07 -0300

    login, cadastro, table projetos

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session ;
+
+class SiteController extends Controller
+{
+    public function login(){
+        return view('site.login');
+    }
+
+    public function logar(Request $request)
+    {
+
+        Session::flush();
+
+        $credentials = [
+            'email' => $request->usuario,
+            'password' => $request->senha
+        ];
+
+        if(Auth::attempt($credentials))
+        {
+
+            $user = Auth::user();
+
+            $usuario_logado = [
+                'id'            => $user->id,
+                'nome'          => $user->nome,
+                'email'          => $user->email,
+                'tipo_id'       => $user->tipo_id,
+            ];
+            
+            Session::put(['usuario' => $usuario_logado]);
+
+            return redirect()->route('site.index');
+
+        }
+
+        return redirect()->back()->withInput()->withErrors(["Usuário ou Senha Incorretos."]);
+    }
+
+    public function cadastro(){
+        return view('site.cadastro');
+    }
+    
+    public function cadastroStore(Request $request){
+
+        User::create([
+            'name'      => $request->usuario,
+            'email'     => $request->email,
+            'password'  => $request->senha,
+            'perfil'    => 2,
+        ]);
+
+        return view('site.login');
+    }
+}
