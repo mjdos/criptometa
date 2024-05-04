f82897018d90529ef87d6bf9b066d82aefec0362
LeonardoAlmeidaRibeiro <leonardo.a.ribeiro.21@gmail.com>
2024-05-04 18:43:20 -0300

    ajuste Meta

--- resources/views/site/projeto/criar.blade.php
+++ resources/views/site/projeto/criar.blade.php
@@ -12,56 +12,85 @@
             <div class="upload-row">
                 <div class="upload-column">
                     <h5>Detalhes do projeto</h5>
-                    <div class="form-field">
-                        <label for="item-category">Tipo de projeto</label>
-                        <select id="tipo_projeto">
-                            <option value="">Seleciona</option>
-                            <option value="pontual">Pontual </option>
-                            <option value="continua">Contínua</option>
-                            <option value="outros">Outros</option>
-                        </select>
-                    </div>
+
                     <div class="form-field">
                         <label for="item-name">Nome do Projeto</label>
                         <input type="text" id="projeto_nome" name="projeto_nome">
                     </div>
+
+                    <div class="form-field">
+                        <label for="item-category">Categoria do Projeto</label>
+                        <select id="projeto_categoria" name="projeto_categoria">
+                            <option value="" disabled selected>Selecione</option>
+                            <option value="tecnologia">Tecnologia</option>
+                            <option value="arte">Arte e Cultura</option>
+                            <option value="design">Design e Moda</option>
+                            <option value="música">Música</option>
+                            <option value="filmes">Filmes e Vídeos</option>
+                            <option value="livros">Livros e Publicações</option>
+                            <option value="alimentacao">Alimentação e Culinária</option>
+                            <option value="esportes">Esportes e Atividades ao Ar Livre</option>
+                            <option value="educação">Educação</option>
+                            <option value="saude">Saúde e Bem-estar</option>
+                            <option value="meio-ambiente">Meio Ambiente</option>
+                            <option value="comunidade">Projetos Comunitários</option>
+                        </select>
+                    </div>
+
                     <div class="form-field">
                         <label for="item-name">Imagem do Projeto</label>
-                        <input type="file" name="projeto_imagem" id="projeto_imagem" class="btn btn-normal btn-dark browse-btn waves-effect waves-button waves-float waves-light">
-                    </div>
-                    <div class="form-field">
-                        <label for="item-description">Descrição do Projeto</label>
-                        <textarea id="projeto_descricao" name="projeto_descricao" cols="30" rows="10"></textarea>
-                    </div>
-                    <div class="form-group">
-                        <div class="form-field">
-                            <label for="item-price">Meta do Projeto</label>
-                            <input type="number" id="projeto_meta">
-                        </div>
-                        <div class="form-field">
-                            <label for="item-category">Categoria do Projeto</label>
-                            <select id="projeto_categoria" name="projeto_categoria">
-                                <option value="tecnologia">Tecnologia</option>
-                                <option value="arte">Arte e Cultura</option>
-                                <option value="design">Design e Moda</option>
-                                <option value="música">Música</option>
-                                <option value="filmes">Filmes e Vídeos</option>
-                                <option value="livros">Livros e Publicações</option>
-                                <option value="alimentacao">Alimentação e Culinária</option>
-                                <option value="esportes">Esportes e Atividades ao Ar Livre</option>
-                                <option value="educação">Educação</option>
-                                <option value="saude">Saúde e Bem-estar</option>
-                                <option value="meio-ambiente">Meio Ambiente</option>
-                                <option value="comunidade">Projetos Comunitários</option>
-                            </select>
+                        {{-- <input type="file" name="projeto_imagem" id="projeto_imagem" class="btn btn-normal btn-dark browse-btn waves-effect waves-button waves-float waves-light"> --}}
+                        <div class="upload-container">
+                            <div class="artwork-upload">
+                                <div class="label">Imagem</div>
+                                <div class="upload-box"><svg class="crumina-icon">
+                                        <use xlink:href="#upload-icon"></use>
+                                    </svg>
+                                    <div class="upload-notice"> Max 120MB, PNG, JPEG, MP3, MP4</div>
+                                    <button
+                                        class="btn btn-normal btn-dark browse-btn waves-effect waves-button waves-float waves-light">Escolher Imagem</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
-                    <input class="btn btn-wide gradient-background" type="submit" value="Upload Item">
-                </div>
+                    <div class="form-field">
+                        <label for="item-description"></label>
+                        <input type="number" id="projeto_meta_1" name="projeto_meta_1">
+                    </div>
+
+                    <div class="form-field">
+                        <label for="item-description"> 2ª Meta</label>
+                        <input type="number" id="projeto_meta_2" name="projeto_meta_2">
+                    </div>
+
+                    <div class="form-field">
+                        <label for="item-description"> 3ª Meta</label>
+                        <input type="number" id="projeto_meta_3" name="projeto_meta_3">
+                    </div>
+
+                    <div class="form-field">
+                        <label for="item-description">4ª Meta</label>
+                        <input type="number" id="projeto_meta_4" name="projeto_meta_4">
+                    </div>
+
+                    <div class="form-field">
+                        <label for="item-description"> 5ª Meta</label>
+                        <input type="number" id="projeto_meta_5" name="projeto_meta_5">
+                    </div>
+
+                    <div class="form-field">
+                        <label for="item-description">Descrição do Projeto</label>
+                        <textarea id="projeto_descricao" name="projeto_descricao" cols="30" rows="10"></textarea>
+
+                    </div>
+              
+
+                <input class="btn btn-wide gradient-background" type="submit" value="Upload Item">
             </div>
-        </form>
     </div>
+    </form>
+</div>
 </div>
 
 
--- resources/views/site/projeto/editar.blade.php
+++ resources/views/site/projeto/editar.blade.php
@@ -5,63 +5,92 @@
 <div class="container">
     <div class="main-content-area">
         <div class="page-title-section">
-            <h2> Editarr <span class="gradient-text">Projeto</span></h2>
+            <h2> Criar <span class="gradient-text">Projeto</span></h2>
         </div>
         <!--  upload item form -->
         <form class="cryptoki-form" id="upload-item-form">
             <div class="upload-row">
                 <div class="upload-column">
                     <h5>Detalhes do projeto</h5>
-                    <div class="form-field">
-                        <label for="item-category">Tipo de projeto</label>
-                        <select id="tipo_projeto">
-                            <option value="">Seleciona</option>
-                            <option value="pontual">Pontual </option>
-                            <option value="continua">Contínua</option>
-                            <option value="outros">Outros</option>
-                        </select>
-                    </div>
+
                     <div class="form-field">
                         <label for="item-name">Nome do Projeto</label>
                         <input type="text" id="projeto_nome_edit" name="projeto_nome_edit">
                     </div>
+
+                    <div class="form-field">
+                        <label for="item-category">Categoria do Projeto</label>
+                        <select id="projeto_categoria_edit" name="projeto_categoria_edit">
+                            <option value="" disabled selected>Selecione</option>
+                            <option value="tecnologia">Tecnologia</option>
+                            <option value="arte">Arte e Cultura</option>
+                            <option value="design">Design e Moda</option>
+                            <option value="música">Música</option>
+                            <option value="filmes">Filmes e Vídeos</option>
+                            <option value="livros">Livros e Publicações</option>
+                            <option value="alimentacao">Alimentação e Culinária</option>
+                            <option value="esportes">Esportes e Atividades ao Ar Livre</option>
+                            <option value="educação">Educação</option>
+                            <option value="saude">Saúde e Bem-estar</option>
+                            <option value="meio-ambiente">Meio Ambiente</option>
+                            <option value="comunidade">Projetos Comunitários</option>
+                        </select>
+                    </div>
+
                     <div class="form-field">
                         <label for="item-name">Imagem do Projeto</label>
-                        <input type="file" name="projeto_imagem_edit" id="projeto_imagem" class="btn btn-normal btn-dark browse-btn waves-effect waves-button waves-float waves-light">
-                    </div>
-                    <div class="form-field">
-                        <label for="item-description">Descrição do Projeto</label>
-                        <textarea id="projeto_descricao_edit" name="projeto_descricao_edit" cols="30" rows="10"></textarea>
-                    </div>
-                    <div class="form-group">
-                        <div class="form-field">
-                            <label for="item-price">Meta do Projeto</label>
-                            <input type="number" id="projeto_meta_edit">
-                        </div>
-                        <div class="form-field">
-                            <label for="item-category">Categoria do Projeto</label>
-                            <select id="projeto_categoria" name="projeto_categoria_edit">
-                                <option value="tecnologia">Tecnologia</option>
-                                <option value="arte">Arte e Cultura</option>
-                                <option value="design">Design e Moda</option>
-                                <option value="música">Música</option>
-                                <option value="filmes">Filmes e Vídeos</option>
-                                <option value="livros">Livros e Publicações</option>
-                                <option value="alimentacao">Alimentação e Culinária</option>
-                                <option value="esportes">Esportes e Atividades ao Ar Livre</option>
-                                <option value="educação">Educação</option>
-                                <option value="saude">Saúde e Bem-estar</option>
-                                <option value="meio-ambiente">Meio Ambiente</option>
-                                <option value="comunidade">Projetos Comunitários</option>
-                            </select>
+                        {{-- <input type="file" name="projeto_imagem" id="projeto_imagem" class="btn btn-normal btn-dark browse-btn waves-effect waves-button waves-float waves-light"> --}}
+                        <div class="upload-container">
+                            <div class="artwork-upload">
+                                <div class="label">Imagem</div>
+                                <div class="upload-box"><svg class="crumina-icon">
+                                        <use xlink:href="#upload-icon"></use>
+                                    </svg>
+                                    <div class="upload-notice"> Max 120MB, PNG, JPEG, MP3, MP4</div>
+                                    <button
+                                        class="btn btn-normal btn-dark browse-btn waves-effect waves-button waves-float waves-light">Escolher Imagem</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
-                    <input class="btn btn-wide gradient-background" type="submit" value="Upload Item">
-                </div>
+                    <div class="form-field">
+                        <label for="item-description"></label>
+                        <input type="number" id="projeto_meta_1_edit" name="projeto_meta_1_edit">
+                    </div>
+
+                    <div class="form-field">
+                        <label for="item-description"> 2ª Meta</label>
+                        <input type="number" id="projeto_meta_2_edit" name="projeto_meta_2_edit">
+                    </div>
+
+                    <div class="form-field">
+                        <label for="item-description"> 3ª Meta</label>
+                        <input type="number" id="projeto_meta_3_edit" name="projeto_meta_3_edit">
+                    </div>
+
+                    <div class="form-field">
+                        <label for="item-description">4ª Meta</label>
+                        <input type="number" id="projeto_meta_4_edit" name="projeto_meta_4_edit">
+                    </div>
+
+                    <div class="form-field">
+                        <label for="item-description"> 5ª Meta</label>
+                        <input type="number" id="projeto_meta_5_edit" name="projeto_meta_5_edit">
+                    </div>
+
+                    <div class="form-field">
+                        <label for="item-description">Descrição do Projeto</label>
+                        <textarea id="projeto_descricao_edit" name="projeto_descricao_edit" cols="30" rows="10"></textarea>
+
+                    </div>
+              
+
+                <input class="btn btn-wide gradient-background" type="submit" value="Upload Item">
             </div>
-        </form>
     </div>
+    </form>
+</div>
 </div>
 
 
