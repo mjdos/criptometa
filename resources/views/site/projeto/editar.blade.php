26749c6a197382e33d18f0773aa3ed4276acb0bc
LeonardoAlmeidaRibeiro <leonardo.a.ribeiro.21@gmail.com>
2024-05-05 17:25:27 -0300

    ajuste perfil

--- resources/views/site/projeto/editar.blade.php
+++ resources/views/site/projeto/editar.blade.php
@@ -8,88 +8,65 @@
             <h2> Criar <span class="gradient-text">Projeto</span></h2>
         </div>
         <!--  upload item form -->
-        <form class="cryptoki-form" id="upload-item-form">
-            <div class="upload-row">
-                <div class="upload-column">
-                    <h5>Detalhes do projeto</h5>
+        <form action="{{ route('projeto.store')}}" method="post" enctype="multipart/form-data"  class="cryptoki-form" id="upload-item-form">
+            @csrf
+            <div class="upload-column">
+                <h5>Detalhes do projeto</h5>
 
-                    <div class="form-field">
-                        <label for="item-name">Nome do Projeto</label>
-                        <input type="text" id="projeto_nome_edit" name="projeto_nome_edit">
-                    </div>
-
-                    <div class="form-field">
-                        <label for="item-category">Categoria do Projeto</label>
-                        <select id="projeto_categoria_edit" name="projeto_categoria_edit">
-                            <option value="" disabled selected>Selecione</option>
-                            <option value="tecnologia">Tecnologia</option>
-                            <option value="arte">Arte e Cultura</option>
-                            <option value="design">Design e Moda</option>
-                            <option value="música">Música</option>
-                            <option value="filmes">Filmes e Vídeos</option>
-                            <option value="livros">Livros e Publicações</option>
-                            <option value="alimentacao">Alimentação e Culinária</option>
-                            <option value="esportes">Esportes e Atividades ao Ar Livre</option>
-                            <option value="educação">Educação</option>
-                            <option value="saude">Saúde e Bem-estar</option>
-                            <option value="meio-ambiente">Meio Ambiente</option>
-                            <option value="comunidade">Projetos Comunitários</option>
-                        </select>
-                    </div>
-
-                    <div class="form-field">
-                        <label for="item-name">Imagem do Projeto</label>
-                        {{-- <input type="file" name="projeto_imagem" id="projeto_imagem" class="btn btn-normal btn-dark browse-btn waves-effect waves-button waves-float waves-light"> --}}
-                        <div class="upload-container">
-                            <div class="artwork-upload">
-                                <div class="label">Imagem</div>
-                                <div class="upload-box"><svg class="crumina-icon">
-                                        <use xlink:href="#upload-icon"></use>
-                                    </svg>
-                                    <div class="upload-notice"> Max 120MB, PNG, JPEG, MP3, MP4</div>
-                                    <button
-                                        class="btn btn-normal btn-dark browse-btn waves-effect waves-button waves-float waves-light">Escolher Imagem</button>
-                                </div>
+                <div class="form-field">
+                    <label for="item-name">Nome</label>
+                    <input type="text" name="nome">
+                </div>
+                <div class="form-field">                        
+                    <div class="upload-container">
+                        <div class="artwork-upload">
+                            <div class="label">Imagem</div>
+                            <div class="upload-box"><svg class="crumina-icon">
+                                    <use xlink:href="#upload-icon"></use>
+                                </svg>
+                                <div class="upload-notice"> Max 120MB, PNG, JPEG, MP3, MP4</div>
+                                <input type="file" name="imagem" class="btn btn-normal btn-dark browse-btn waves-effect waves-button waves-float waves-light"></input>
                             </div>
                         </div>
                     </div>
+                </div>
+                <div class="form-field">
+                    <label>Valor para Arrecadar</label>
+                    <input type="number" name="valor">
+                </div>
+                <div class="form-field">
+                    <label>1ª Meta</label>
+                    <input type="text" name="meta_1">
+                </div>
 
-                    <div class="form-field">
-                        <label for="item-description"></label>
-                        <input type="number" id="projeto_meta_1_edit" name="projeto_meta_1_edit">
-                    </div>
+                <div class="form-field">
+                    <label>2ª Meta</label>
+                    <input type="text" name="meta_2">
+                </div>
 
-                    <div class="form-field">
-                        <label for="item-description"> 2ª Meta</label>
-                        <input type="number" id="projeto_meta_2_edit" name="projeto_meta_2_edit">
-                    </div>
+                <div class="form-field">
+                    <label>3ª Meta</label>
+                    <input type="text" name="meta_3">
+                </div>
 
-                    <div class="form-field">
-                        <label for="item-description"> 3ª Meta</label>
-                        <input type="number" id="projeto_meta_3_edit" name="projeto_meta_3_edit">
-                    </div>
+                <div class="form-field">
+                    <label>4ª Meta</label>
+                    <input type="text" name="meta_4">
+                </div>
 
-                    <div class="form-field">
-                        <label for="item-description">4ª Meta</label>
-                        <input type="number" id="projeto_meta_4_edit" name="projeto_meta_4_edit">
-                    </div>
+                <div class="form-field">
+                    <label>5ª Meta</label>
+                    <input type="text" name="meta_5">
+                </div>
 
-                    <div class="form-field">
-                        <label for="item-description"> 5ª Meta</label>
-                        <input type="number" id="projeto_meta_5_edit" name="projeto_meta_5_edit">
-                    </div>
-
-                    <div class="form-field">
-                        <label for="item-description">Descrição do Projeto</label>
-                        <textarea id="projeto_descricao_edit" name="projeto_descricao_edit" cols="30" rows="10"></textarea>
-
-                    </div>
-              
-
-                <input class="btn btn-wide gradient-background" type="submit" value="Upload Item">
+                <div class="form-field">
+                    <label>Descrição</label>
+                    <textarea name="descricao" cols="30" rows="10"></textarea>
+                </div>
+                <input class="btn btn-wide gradient-background" type="submit" value="Salvar">
             </div>
+        </form>
     </div>
-    </form>
 </div>
 </div>
 
--- resources/views/site/usuario/projeto.blade.php
+++ resources/views/site/usuario/projeto.blade.php
@@ -1,6 +1,6 @@
 @include('site.layouts.header')
 @php
-    $usuario = Session::get('usuario');
+$usuario = Session::get('usuario');
 @endphp
 <!-- main content area -->
 <div class="primary-content-area container content-padding grid-left-sidebar">
@@ -24,9 +24,8 @@
                                     </svg>
                                 </div>
                                 <div class="create-item-info">
-                                    <div class="small-title">Create a New Item!
-                                    </div>
-                                    <div class="description">Click here to start</div>
+                                    <div class="small-title">Crie um Projeto</div>
+                                    <div class="description">  <a href="{{route('projeto.criar')}}" class="btn btn-small-wide btn-dark"> Clique aqui para começa!</a></div>
                                 </div>
                             </div>
                             <div class="create-item-post-content">
@@ -34,60 +33,41 @@
                         </div>
                     </div>
 
-                    
+                    @foreach($projetos as $projeto)
                     <div class="featured-item v5">
                         <div class="featured-item-wrapper">
                             <div class="featured-item-content">
-                                <button class="btn btn-small-wide btn-dark">Edit</button>
-                                <div class="fav-counter"><svg class="crumina-icon">
-                                        <use xlink:href="#heart-icon"></use>
-                                    </svg><span class="count">22</span></div>
                                 <div class="featured-item-image">
                                     <a href="05-product.html">
-                                        <img src="img/content/items/featured-item-29.png" alt=""></a>
+                                        <img src="@if(isset($projeto->imagem))
+                                        {{$projeto->imagem }}" alt=""></a>
+                                        @else
+                                        ?? img/content/items/featured-item-29.png
+                                        @endif
                                 </div>
                                 <div class="featured-item-info">
-                                    <div class="item-category social-graphics">
-                                        Social graphics
-                                    </div>
+                                    
                                     <div class="title">
-                                        <a href="05-product.html">Youtube Gaming Thumbnails Pack 30</a>
+                                        <a href="05-product.html">{{$projeto->nome}}</a>
                                     </div>
-                                    <div class="item-meta"><span class="avatar box-26"><a
-                                                href="/cryptoki-html/"><img src="img/avatar.png" alt=""></a><span
-                                                class="verified"><svg class="crumina-icon">
-                                                    <use xlink:href="#check-icon"></use>
-                                                </svg></span></span>@DexterStark</div>
+                                    <div class="item-meta"><span class="avatar box-26"><a href="/cryptoki-html/"><img src="img/avatar.png" alt=""></a><span class="verified">
+                                                </span></span>{{$projeto->autor->name}}</div>
                                 </div>
                             </div>
                             <div class="featured-item-post-content">
-                                <div class="item-rating">
-                                    <span class="filled"><svg class="crumina-icon">
-                                            <use xlink:href="#star2-icon"></use>
-                                        </svg></span>
-                                    <span class="filled"><svg class="crumina-icon">
-                                            <use xlink:href="#star2-icon"></use>
-                                        </svg></span>
-                                    <span class="filled"><svg class="crumina-icon">
-                                            <use xlink:href="#star2-icon"></use>
-                                        </svg></span>
-                                    <span class="filled"><svg class="crumina-icon">
-                                            <use xlink:href="#star2-icon"></use>
-                                        </svg></span>
-                                    <span><svg class="crumina-icon">
-                                            <use xlink:href="#star2-icon"></use>
-                                        </svg></span>
-                                </div>
-                                <div class="item-price">$8.00</div>
+                                
+                                <div class="item-price">${{$projeto->carteira}}</div>
                             </div>
                         </div>
                     </div>
 
+                    @endforeach
+
                 </div>
             </div>
- 
+
         </div>
-        
+
     </div>
 
 </div>
